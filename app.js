007ed1011c9d6c77730bf8d48acb1b19fcab6ce9
Rutuja Jadhav <rutujajadhav3125@gmail.com>
2026-05-09 12:26:24 +0530

    Task 1 completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/submit", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+
+    res.render("result", { name, email });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
